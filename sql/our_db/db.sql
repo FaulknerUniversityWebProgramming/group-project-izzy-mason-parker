586074dc889fb0e8d26e0e56658a1e97624aa609
Mason Simonton <711953@faulkner.edu>
2020-11-06 17:32:17 +0000

    created .sql and two .dump files

---
+++ sql/our_db/db.sql
@@ -0,0 +1,97 @@
+-- MySQL dump 10.13  Distrib 5.5.59, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: users
+-- ------------------------------------------------------
+-- Server version	5.5.59-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `users`
+--
+
+DROP DATABASE IF EXISTS db;
+CREATE DATABASE IF NOT EXISTS db;
+USE db;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS user,
+                     item;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE user (
+  user_id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(25) NOT NULL,
+  password varchar(25) NOT NULL,
+  email varchar(50) NOT NULL,
+  phone int(10) DEFAULT NULL,
+  address varchar(100) NOT NULL,
+  fname varchar(30) NOT NULL,
+  lname varchar(40) NOT NULL,
+  PRIMARY KEY (user_id),
+  UNIQUE KEY username (username),
+  UNIQUE KEY address (address)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+CREATE TABLE item (
+    item_id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11) NOT NULL,
+    iname VARCHAR(50) NOT NULL,
+    category VARCHAR(20) NOT NULL,
+    price DOUBLE(15,2) NOT NULL DEFAULT 0,
+    purchased BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY(item_id),
+    FOREIGN KEY (user_id)  REFERENCES user (user_id)    ON DELETE CASCADE
+    
+);
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES user WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+
+SELECT 'LOADING user' as 'INFO';
+source group-project-izzy-mason-parker/sql/our_db/load_user.dump ;
+source group-project-izzy-mason-parker/sql/our_db/load_item.dump ;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES item WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+
+SELECT 'LOADING user' as 'INFO';
+source group-project-izzy-mason-parker/sql/our_db/load_user.dump ;
+source group-project-izzy-mason-parker/sql/our_db/load_item.dump ;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-05 22:32:43
