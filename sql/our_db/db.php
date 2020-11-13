5eeb289b35616b0cc78dcf4ecef29db7c9eb8e54
Mason Simonton <711953@faulkner.edu>
2020-11-13 18:05:52 +0000

    fixed descriptions

--- sql/our_db/db.php
+++ sql/our_db/db.php
@@ -8,7 +8,7 @@
     try{
         $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
         $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'DB connection successful!';
+        //echo 'DB connection successful!';
     }
     catch(PDOException $e){
         echo 'DB connection failed '.$e->getMessage();
